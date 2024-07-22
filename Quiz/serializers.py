a1ad50ea368f878ff512a727bc956dbdfcb721a3
maxwell <246339@edu.p.lodz.pl>
2024-07-22 20:28:24 +0200

    fix: validate in QuestionSerializer

--- Quiz/serializers.py
+++ Quiz/serializers.py
@@ -1,32 +1,41 @@
 from rest_framework import serializers
 from .models import Quiz, Question
-import sys
+import logging
+
+logger = logging.getLogger('django')
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ['text', 'is_open_ended', 'answers']
 
-        def validate(self, attrs):
-            answer_count = len(attrs['answers'])
-            if answer_count == 0:
-                raise serializers.ValidationError("A question must have at least 1 answer if it is open ended or 2 answers if it is closed")
-            if answer_count == 1 and not attrs['is_open_ended']:
-                raise serializers.ValidationError("A closed question must have at least 2 answers")
+    def validate(self, attrs):
+        if 'is_open_ended' not in attrs.keys() or attrs['is_open_ended'] is False:
+            is_open_ended = False
+        else:
+            is_open_ended = True
 
-            print(attrs['is_open_ended'], file=sys.stderr)
-            if attrs['is_open_ended'] is False:
-                raise serializers.ValidationError("A question must have a type [open ended or closed]")
+        logger.info(is_open_ended)
 
-            return attrs
+        answer_count = len(attrs['answers'])
+        logger.info(attrs)
+        if answer_count == 0:
+            raise serializers.ValidationError("A question must have at least 1 answer if it is open ended or 2 answers if it is closed")
+        if answer_count == 1 and not is_open_ended:
+            raise serializers.ValidationError("A closed question must have at least 2 answers")
+
+        return attrs
 
 
 class QuizSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True, allow_null=True)
 
     def validate(self, attrs):
+        logger.info(attrs)
+
         if len(attrs['questions']) < 1:
             raise serializers.ValidationError("A quiz must have at least 1 question")
+
         return attrs
 
     class Meta:
@@ -39,6 +48,3 @@ class QuizSerializer(serializers.ModelSerializer):
         for question in questions:
             Question.objects.create(quiz=quiz, **question)
         return quiz
-
-
-
